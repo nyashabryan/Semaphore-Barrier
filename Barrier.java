77a7d29f490971b06b2489da1b4a8f15f3a83336
Nyasha Bryan <nyashabryan23@gmail.com>
2019-04-20 23:08:55 +0200

    Git initialize

---
+++ Barrier.java
@@ -0,0 +1,16 @@
+package BarrierS;
+import java.util.concurrent.Semaphore;
+
+public class Barrier {
+	
+	// add missing variables
+	
+	Barrier(int n) {
+		// complete this constructor
+	}
+	
+	public void b_wait() throws InterruptedException{
+		// this is the only additional method you will need to complete
+	}
+
+}
