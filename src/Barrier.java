7755eeda27caae3cd3ee8d442e3feaac75d1f24d
Nyasha Bryan <nyashabryan23@gmail.com>
2019-04-23 08:50:45 +0200

    Removed atomic integer class

--- src/Barrier.java
+++ src/Barrier.java
@@ -1,24 +1,21 @@
 package BarrierS;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Barrier {
 	
 	private final Semaphore barrierSem;
-	private static AtomicInteger count;
+	private static int count = 0;
 	private static int n;
 	
 	protected Barrier(int n) {
 		barrierSem = new Semaphore(n);
 		barrierSem.drainPermits();
 		Barrier.n = n;
-		count = new AtomicInteger(0);
 	}
 	
 	public void b_wait() throws InterruptedException{
 		// this is the only additional method you will need to complete
-		count.incrementAndGet();
-		if(count.get() == n){
+		if(count== n){
 			barrierSem.release();
 		}
 		barrierSem.acquire();
